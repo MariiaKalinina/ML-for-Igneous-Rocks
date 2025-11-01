81324c6da332ed6b1237592de79d099448c1887d
MariiaKalinina <101808180+MariiaKalinina@users.noreply.github.com>
2025-11-01 13:44:47 +0300

    Create paths.py

---
+++ config/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path('/content/R_lithology_project')
+DATA_DIR = PROJECT_ROOT / 'data'
+SRC_DIR = PROJECT_ROOT / 'src'
+NOTEBOOKS_DIR = PROJECT_ROOT / 'notebooks'
+
+RAW_DATA_PATH = DATA_DIR / 'R_dataset.xlsx'
+PROCESSED_DATA_DIR = DATA_DIR / 'processed_data'
